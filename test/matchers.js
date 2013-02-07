c5ec84045197d86724f1ed5f457c5885d7152331
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-02-07 11:58:01 +0400

    add array methods matching

--- test/matchers.js
+++ test/matchers.js
@@ -17,3 +17,29 @@ describe('matcher factory', function() {
         matcherFactory('custom', { fn : function() {} }).should.be.an.instanceof(CustomMatcher);
     });
 });
+
+describe('method matcher', function() {
+    it('should throw exception if no method in params', function() {
+        var fn = function() {
+                matcherFactory('method', {});
+            };
+        fn.should.throw('method expected');
+    });
+
+    it('should match method from string', function() {
+        var matcher = matcherFactory('method', { method : 'get' });
+        matcher.match({ method : 'get' }).should.be.true;
+        matcher.match({ method : 'post' }).should.be.false;
+    });
+
+    it('should match methods from array', function() {
+        var matcher = matcherFactory('method', { method : ['get', 'post'] });
+        matcher.match({ method : 'get' }).should.be.true;
+        matcher.match({ method : 'post' }).should.be.true;
+    });
+
+    it('should match default method', function() {
+        var matcher = matcherFactory('method', { method : 'get' });
+        matcher.match({}).should.be.true;
+    });
+});
